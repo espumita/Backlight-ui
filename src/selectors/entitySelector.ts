ed86fb0d2e4711d2b20a546c178afbc8e7ae24d4
Espumita <davidfalconnaranjo@gmail.com>
2020-09-08 17:24:01 +0100

    Green: get multiple entities sorted alphabetically when they are configured in the api

--- src/selectors/entitySelector.ts
+++ src/selectors/entitySelector.ts
@@ -13,12 +13,22 @@ function entitiesFrom(paths: PathsObject): Entity[]{
     const pathNames = Object.keys(paths)
     if (pathNames.length == 0) return []
     return pathNames.map(pathName => entityFrom(pathName))
+                .sort(shortAlphabetically())
 }
+
 
 function entityFrom(pathName: string) : Entity {
     const pathSplited = pathName.split('.')
     const entityName = pathSplited[pathSplited.length -1]
     return {
        name: entityName
+    }
+}
+
+function shortAlphabetically(): (a: Entity, b: Entity) => number {
+    return function (a, b) {
+        var textA = a.name.toUpperCase()
+        var textB = b.name.toUpperCase()
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0
     }
 }
--- tests/selectors/entitySelector.spec.ts
+++ tests/selectors/entitySelector.spec.ts
@@ -2,8 +2,12 @@ import { entitySelector } from '../../src/selectors/entitySelector'
 import { storeBuilder } from '../mockStoreBuilder'
 import { openApiConfigurationBuilder } from '../openApiConfigurationBuilder'
 
-const anEntityName = 'ExampleEntity'
-const aPathName = '/api/type/Backlight.Sample.Web.Api.Entities.ExampleEntity'
+const anEntityName = 'aEntity'
+const aPathName = `/api/type/Backlight.Sample.Web.Api.Entities.${anEntityName}`
+const anotherEntityName = 'aEntity2'
+const anotherPathName = `/api/type/Backlight.Sample.Web.Api.Entities.${anotherEntityName}`
+const anotherOneEntityName = 'zEntity'
+const anotherOnePathName = `/api/type/Backlight.Sample.Web.Api.Entities.${anotherOneEntityName}`
 
 describe('Entity selectors', () => {
 
@@ -27,6 +31,24 @@ describe('Entity selectors', () => {
 
         expect(selectedEntities).toHaveLength(1)
         expect(selectedEntities[0].name).toBe(anEntityName)
+    })
+
+    test('get multiple entities sorted alphabetically when they are configured in the api', () => {
+        const store = storeBuilder()
+            .WithOpenApiConfiguration(openApiConfigurationBuilder()
+                .WithPath(anotherOnePathName)
+                .WithPath(aPathName)
+                .WithPath(anotherPathName)
+                .build()
+            )
+            .buildState()
+        
+        const selectedEntities = entitySelector.resultFunc(store.openApi.configuration)
+
+        expect(selectedEntities).toHaveLength(3)
+        expect(selectedEntities[0].name).toBe(anEntityName)
+        expect(selectedEntities[1].name).toBe(anotherEntityName)
+        expect(selectedEntities[2].name).toBe(anotherOneEntityName)
     })
 
 })

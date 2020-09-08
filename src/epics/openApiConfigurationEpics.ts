658113a0dce4cc00c94131d00640847279f40cdf
Espumita <davidfalconnaranjo@gmail.com>
2020-09-08 11:50:06 +0100

    Refactor: use redux-observable ofType operator

--- src/epics/openApiConfigurationEpics.ts
+++ src/epics/openApiConfigurationEpics.ts
@@ -1,7 +1,7 @@
-import { Epic } from 'redux-observable'
+import { Epic, ofType } from 'redux-observable'
 import { Action } from 'redux'
 import { Store } from '../store/store'
-import { filter, exhaustMap, map, catchError } from "rxjs/operators";
+import { exhaustMap, map, catchError } from "rxjs/operators";
 import { from, of } from "rxjs";
 import { LOAD_OPEN_API_CONFIGURATION } from '../actions/actionsTypes';
 import openApiClient from '../clients/openApiClient'
@@ -9,9 +9,9 @@ import { loadOpenApiConfigurationSuccess, loadOpenApiConfigurationError } from '
 import { OpenAPIObject } from 'openapi3-ts';
 
 export const loadOpenApiConfigurationEpic: Epic<Action, Action, Store> = (action$, store) => action$.pipe(
-  filter(action => action.type === LOAD_OPEN_API_CONFIGURATION), //check ofType operator
-  exhaustMap(
-    action => from(openApiClient.loadOpenApiConfiguration()).pipe(
+  ofType(LOAD_OPEN_API_CONFIGURATION),
+  exhaustMap(action =>
+    from(openApiClient.loadOpenApiConfiguration()).pipe(
       map((data: OpenAPIObject) => loadOpenApiConfigurationSuccess(data)),
       catchError((error: any) =>  of(loadOpenApiConfigurationError()))
     )

fc53e19de03d2937cd1ce6b33c1c4b01232e2a3d
Espumita <davidfalconnaranjo@gmail.com>
2020-11-07 01:48:44 +0000

    Add some styles to entity list

--- src/components/entityList/EntityList.tsx
+++ src/components/entityList/EntityList.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './entityList.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { currentEntityIdsSelector } from '../../selectors/currentEntitySelector'
+import { animationFrameScheduler } from 'rxjs'
 
 const EntityList = () => {
   const currentEntityIds = useSelector(currentEntityIdsSelector)
@@ -25,7 +26,21 @@ function entityIdItem(entityId: string, index: Number, dispatch: Function) {
       key={`entity-list-entity-id-item-${index}`}
       onClick={() =>{ console.log("Hi from entityIdItem")}}
     >
-      {entityId}
+      {entityPreview(entityId)}
+    </div>
+  )
+}
+
+function entityPreview(entityId: string){
+  const list = ["id","name","date","second"]
+  return list.map(x => a(x, entityId))
+}
+
+function a(x: string, entityId: string){
+  return(
+    <div className={`entity-list-entity-id-item-property`}>
+      <div className={`entity-list-entity-id-item-property-name`}>{x}</div>
+      <div>: {x === "id" ? entityId : ""}</div>
     </div>
   )
 }

d1afd01038cef9d6baa40603aa6ddc28cef57ccf
Espumita <davidfalconnaranjo@gmail.com>
2021-03-06 17:29:50 +0000

    Move clients config responsability to backend

--- src/clients/entityClient.ts
+++ src/clients/entityClient.ts
@@ -1,13 +1,14 @@
 import axios from 'axios'
 import { Entity } from '../store/Entity'
+import clientConfig from './clientConfig'
 
 export default {
     getAll: (entity: Entity) => {
-        return axios.get(`https://localhost:44349/back/api/types/${entity.name}/all`)
+        return axios.get(`${clientConfig.server}/${clientConfig.routePrefix}/api/types/${entity.name}/all`)
                     .then(response => response.data)
     },
     get: (entity: Entity, entityId: string) => {
-        return axios.get(`https://localhost:44349/back/api/types/${entity.name}/entities/${entityId}`)
+        return axios.get(`${clientConfig.server}/${clientConfig.routePrefix}/api/types/${entity.name}/entities/${entityId}`)
                     .then(response => response.data)
     }
 }

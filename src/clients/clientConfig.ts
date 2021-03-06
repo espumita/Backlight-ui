e7b6e4bcb0ac16ca15bd4162ac65e6c04010bc50
Espumita <davidfalconnaranjo@gmail.com>
2021-03-06 17:51:38 +0000

    Update to 1.0.5 and check for nulls in windows properties

--- src/clients/clientConfig.ts
+++ src/clients/clientConfig.ts
@@ -1,5 +1,5 @@
 
 export default {
-    server: (window as any).backlightNetConfig.server,
-    routePrefix: (window as any).backlightNetConfig.routePrefix
+    server: (window as any).backlightNetConfig?.server,
+    routePrefix: (window as any).backlightNetConfig?.routePrefix
 }

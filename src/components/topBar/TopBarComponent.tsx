1615d6321e98cdeab5e7096c0646a73123849c22
Espumita <davidfalconnaranjo@gmail.com>
2021-03-22 21:42:10 +0000

    Update TopBar version to 1.0.8
    This should be parameterized in build

--- src/components/topBar/TopBarComponent.tsx
+++ src/components/topBar/TopBarComponent.tsx
@@ -8,7 +8,7 @@ const TopBar = () => {
           <div className="top-bar-content">
             <img src={TopBarImage} className="top-bar-image" alt="Backlight UI" />
             <div className="top-bar-tittle">Backlight</div>
-            <div className="top-bar-version">1.0.7</div>
+            <div className="top-bar-version">1.0.8</div>
           </div>
       </div>
     )
